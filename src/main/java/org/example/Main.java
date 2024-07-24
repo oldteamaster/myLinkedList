ed8e2607129977d49ff7570307aad8728c59a5bf
Pavlo Olenchenko <teamaster72@gmail.com>
2024-07-24 09:11:19 +0300

    HW_2.2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+myLinkedList list = new myLinkedList();
+
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(20);
+        list.add(25);
+        list.display();
+        System.out.println("Size: " + list.size());
+        System.out.println("Видалення елементу index = 2");
+        list.remove(2);
+        list.display();
+        System.out.println("Size: " + list.size());
+        System.out.println("Отримати елемент index = 3");
+        System.out.println("Елемент index 3: " + list.get(3));
+        System.out.println("Очищення колекції");
+        list.clear();
+        System.out.println("Size: " + list.size());
+
+
+
+
+    }
+}
